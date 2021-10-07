c76c78390452a0a1971339cc1be2ea8f6cc2978a
Sandini <sandinipitawala1@gmail.com>
2021-10-07 23:40:06 +0530

    pms backend

---
+++ src/main/java/com/nsss/pizzamanagementsystembackend/repository/CrustRepository.java
@@ -0,0 +1,11 @@
+package com.nsss.pizzamanagementsystembackend.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CrustRepository extends MongoRepository<Crust, String> {
+    Optional<Crust> findByName(String crustName);
+    List<Crust> findAllByName(String crustName);
+}
